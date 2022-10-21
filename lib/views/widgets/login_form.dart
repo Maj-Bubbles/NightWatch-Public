d38d632cc7708b96d198e32193d684f3c03cac87
Emile <emilebotes01@icloud.com>
2022-10-21 13:12:08 +0200

    Added a file in each directory

---
+++ lib/views/widgets/login_form.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class LoginForm extends StatefulWidget {
+  const LoginForm({super.key});
+
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
