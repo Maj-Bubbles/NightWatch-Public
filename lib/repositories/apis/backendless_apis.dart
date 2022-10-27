0911d6bbd4c63d109af57266374176ad26ae79a8
kamva9697 <kamva9697@gmail.com>
2022-10-27 22:40:42 +0200

    feat: Add Database and RealTimeDB apis
    Add the above apis with their burrel file

---
+++ lib/repositories/apis/backendless_apis.dart
@@ -0,0 +1,7 @@
+/// This is the Backendless apis burrell file
+/// all apis are exported herein, so that one
+/// file is imported across the project.
+
+export 'backendless_database_api.dart';
+export 'backendless_realtimeDB_api.dart';
+export 'backendless_user_api.dart';
---
+++ lib/repositories/apis/backendless_database_api.dart
@@ -0,0 +1,11 @@
+import 'package:backendless_sdk/backendless_sdk.dart';
+import 'package:nightwatch/models/models.dart';
+
+class BackendlessDatabaseApi {
+  static Backendless backendlessDatabaseApi = Backendless();
+
+  static void saveSingleReport(Report report) {}
+
+  static dynamic searchForReports(String keyword) {}
+
+}
