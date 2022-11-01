586412993e92ff35dcb06eda0b9d88ca00fb7902
kamva9697 <kamva9697@gmail.com>
2022-11-01 20:03:47 +0200

    feat: Add Database API
    Implements the appropriate apis for storing and retrieving reports

--- lib/repositories/apis/backendless_database_api.dart
+++ lib/repositories/apis/backendless_database_api.dart
@@ -1,11 +1,48 @@
 import 'package:backendless_sdk/backendless_sdk.dart';
+import 'package:flutter/services.dart';
 import 'package:nightwatch/models/models.dart';
 
 class BackendlessDatabaseApi {
-  static Backendless backendlessDatabaseApi = Backendless();
+  Backendless backendlessDatabaseApi = Backendless();
 
-  static void saveSingleReport(Report report) {}
+  void saveSingleReport(Report report) {
+    var serializedReport = {
+      "Username": report.username,
+      "Title": report.title,
+      "Description": report.description,
+      "DateTime": report.dateTime,
+      "Alerted": report.isAlerted,
+      "Acknowledged": report.isAcknowledged,
+      "Imminent": report.isImminent,
+      "LocationData": report.locationData,
+      "Media": {report.dateTime: report.media},
+    };
+    Backendless.data.of("Report").save(serializedReport).catchError(
+        (error, stackTrace) =>
+            _handleError(error, stackTrace, apiName: "saveSingleReport"));
+  }
 
-  static dynamic searchForReports(String keyword) {}
+  dynamic retrieveUserReports(String keyword) {
+    var query = DataQueryBuilder()..whereClause = "Reports";
+    return Backendless.data.of("Users").find(query).catchError((error, stackTrace) =>
+        _handleError(error, stackTrace, apiName: "retrieveUserReports"));
+  }
 
+  /// Code reuse: Handle API Errors and print details to the console
+  /// throws an Exception to handled by a user of the api
+  _handleError(PlatformException error, StackTrace stackTrace,
+      {required String apiName}) {
+    _logException(error, stackTrace, apiName);
+    throw BackendlessException(error.details, error.code as int);
+  }
+
+  /// Prints error information of a Backendless API error
+  /// to the console for ease of debugging
+  void _logException(
+      PlatformException error, StackTrace stackTrace, String apiName) {
+    print("Backendless API Error: $apiName");
+    print("exception code: ${error.code}");
+    print("exception details: ${error.details}");
+    print("stacktrace: $stackTrace");
+  }
 }
