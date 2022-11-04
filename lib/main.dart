18f577203923a8cf5752ea5f3567990d26253b45
Solomon Phelembe <chrisspk803@gmail.com>
2022-11-04 16:56:53 +0200

    feat: Added two files under services repo and edited main.dart file.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,28 @@
+import 'package:flutter/material.dart';
 import 'package:nightwatch/init.dart';
-import 'package:nightwatch/views/pages/login_page.dart';
+import 'package:nightwatch/routes/route_manager.dart';
+import 'package:nightwatch/services/locator_service.dart';
+import 'package:nightwatch/services/navigation_and_dialog_service.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   InitApp.initializeApp();
-  const LoginPage();
+  setupLocator();
+  runApp(
+    const NightWatchApp(),
+  );
+}
+
+class NightWatchApp extends StatelessWidget {
+  const NightWatchApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      navigatorKey: navigatorKey,
+      onGenerateRoute: RouteManager.onGenerateRoute,
+      initialRoute: RouteManager.loginPage,
+    );
+  }
 }
