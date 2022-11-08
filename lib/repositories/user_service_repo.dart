b0802fcc57588e7e3059ac28f2532d2e215da288
kamva9697 <kamva9697@gmail.com>
2022-11-08 21:57:57 +0200

    refactor: Adapt Interface to suit use case
    Added appropriate return types and parameter types for applicable use cases.

--- lib/repositories/user_service_repo.dart
+++ lib/repositories/user_service_repo.dart
@@ -1,18 +1,19 @@
+import 'package:backendless_sdk/backendless_sdk.dart';
 import 'package:nightwatch/models/models.dart';
 
 abstract class UserServiceRepo {
 
-  void registerUser(User user);
+  Future<BackendlessUser> registerUser(User user);
 
-  void signInUser(User user);
+  void signInUser({required String email, required String password});
 
   void logoutUser(User user);
 
-  void forgotPassword(User user);
+  Future<void> resetPassword(String email);
 
   void checkIfUserLogged(User user);
 
-  void checkIfUserExists(User user);
+  Future<bool> checkIfUserExists(String email);
 
   void checkIfUserIsAdmin(User user);
 }
