ad33c5c1e6f3efcfa42b815fb821f78ae566d90a
Emile <emilebotes01@icloud.com>
2022-11-08 11:24:35 +0200

    feat: Added screens and widgets from loginPage branch

--- lib/views/pages/login_page.dart
+++ lib/views/pages/login_page.dart
@@ -1,11 +1,31 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+
+import '../widgets/login_form.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        automaticallyImplyLeading: false,
+        title: const Text(
+          'Login',
+          style: TextStyle(fontSize: 40, color: Colors.white),
+        ),
+      ),
+      body: Stack(
+        children: const [
+          SingleChildScrollView(
+            child: LoginScreenForm(),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/views/widgets/login_form.dart
+++ lib/views/widgets/login_form.dart
@@ -1,16 +1,105 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:nightwatch/miscellaneous/configuration.dart';
+import 'package:nightwatch/miscellaneous/validators.dart';
+import 'package:nightwatch/view_models/user_view_model.dart';
+import 'package:provider/provider.dart';
 
-class LoginForm extends StatefulWidget {
-  const LoginForm({super.key});
+class LoginScreenForm extends StatefulWidget {
+  const LoginScreenForm({
+    Key? key,
+  }) : super(key: key);
 
   @override
-  State<LoginForm> createState() => _LoginFormState();
+  State<LoginScreenForm> createState() => _LoginScreenFormState();
 }
 
-class _LoginFormState extends State<LoginForm> {
+class _LoginScreenFormState extends State<LoginScreenForm> {
+  late TextEditingController emailController;
+  late TextEditingController passwordController;
+  @override
+  void initState() {
+    super.initState();
+    emailController = TextEditingController();
+    passwordController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SingleChildScrollView(
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Form(
+            key: context.read<UserViewModel>().loginFormKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Card(
+                  clipBehavior: Clip.antiAlias,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                  elevation: 20,
+                  child: Image.asset('lib/media/Login.png'),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  'Sign in to Continue',
+                  style: GoogleFonts.dmSans(
+                    fontSize: 30,
+                    color: const Color(0xFFFFFFFF),
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(
+                  height: 60,
+                ),
+                TextFormField(
+                  controller: emailController,
+                  validator: validateEmail,
+                  style: const TextStyle(color: Colors.black),
+                  decoration: formDecoration('Email Address'),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  controller: passwordController,
+                  validator: validatePassword,
+                  style: const TextStyle(color: Colors.black),
+                  decoration: formDecoration('Password'),
+                ),
+                const SizedBox(
+                  height: 60,
+                ),
+                MaterialButton(
+                  height: 50,
+                  minWidth: 150,
+                  onPressed: () {},
+                  color: const Color(0xFFFBA00A),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40)),
+                  child: const Text(
+                    'Log In',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
