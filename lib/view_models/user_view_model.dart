f85fa4ead499c3815dfa611c23737abf25795903
kamva9697 <kamva9697@gmail.com>
2022-11-10 21:42:07 +0200

    feat: Implement UserViewModel

--- lib/view_models/user_view_model.dart
+++ lib/view_models/user_view_model.dart
@@ -1,16 +1,13 @@
 import 'package:backendless_sdk/backendless_sdk.dart';
-import 'package:flutter/material.dart';
 import 'package:nightwatch/models/models.dart';
 import 'package:nightwatch/repositories/repositories.dart';
 import 'package:nightwatch/services/services.dart';
+import 'package:nightwatch/view_models/base_view_model.dart';
+import 'package:nightwatch/view_models/error_handling.dart';
 
-class UserViewModel with ChangeNotifier {
+class UserViewModel extends BaseViewModel {
   late UserServiceRepo _userService;
   late BackendlessUser _currentUser;
-  // The following variables are public because they
-  // final thus have no use case for getters and setters
-  late final registerFormKey;
-  late final loginFormKey;
 
   UserServiceRepo get userService => _userService;
   BackendlessUser get currentUser => _currentUser;
@@ -18,15 +15,57 @@ class UserViewModel with ChangeNotifier {
   UserViewModel() {
     _userService = UserService();
     _currentUser = BackendlessUser();
-    registerFormKey = GlobalKey<FormState>();
-    loginFormKey = GlobalKey<FormState>();
   }
 
-  void registerUser(User user) {}
-  void signInUser(User user) {}
-  void logoutUser(User user) {}
-  void forgotPassword(User user) {}
-  void checkIfUserIsLoggedIn(User user) {}
-  void checkIfUserExists(User user) {}
-  void checkIfUserIsAdmin(User user) {}
+  Future<void> registerUser(User user) async {
+    try {
+      setState(ViewState.Busy);
+      _currentUser =
+          await _userService.registerUser(user);
+      setState(ViewState.Success);
+    } on UserAPIException catch (error) {
+      setErrorDialog(error);
+    }
+  }
+
+  Future<void> signInUser({required String email, required String password}) async {
+    try {
+      checkIfUserIsLoggedIn();
+      setState(ViewState.Busy);
+      _currentUser =
+      await _userService.signInUser(email: email, password: password);
+      setState(ViewState.Success);
+    } on UserAPIException catch (error) {
+      setErrorDialog(error);
+    }
+  }
+
+  Future<void> logoutUser() async {
+    try {
+      setState(ViewState.Busy);
+      await _userService.logoutUser();
+      setState(ViewState.Success);
+    } on UserAPIException catch (error) {
+      setErrorDialog(error);
+    }
+  }
+  Future<void> forgotPassword(String email) async {
+    try {
+      setState(ViewState.Busy);
+      await _userService.resetPassword(email: email);
+      setState(ViewState.Success);
+    } on UserAPIException catch (error) {
+      setErrorDialog(error);
+    }
+  }
+  Future<bool> checkIfUserIsLoggedIn() async {
+      return await _userService.checkIfUserLogged();
+  }
+  Future<bool> checkIfUserExists(String email) async {
+   return await _userService.checkIfUserExists(email: email);
+  }
+  Future<bool> checkIfUserIsAdmin() async {
+    return await _userService.checkIfUserIsAdmin();
+  }
+
 }
