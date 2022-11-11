7c40cd73c90798e654a18352aaea9dd8b60f3663
kamva9697 <kamva9697@gmail.com>
2022-11-11 20:59:11 +0200

    refactor: Use consistent encapsulation within View models
    use encapsulation as in user view model

--- lib/view_models/reports_view_model.dart
+++ lib/view_models/reports_view_model.dart
@@ -1,22 +1,60 @@
+import 'package:backendless_sdk/backendless_sdk.dart';
 import 'package:nightwatch/repositories/reports_repository.dart';
-import 'package:nightwatch/services/reports_service.dart';
+import 'package:nightwatch/services/services.dart';
+import 'package:nightwatch/view_models/base_view_model.dart';
 import 'package:nightwatch/view_models/user_view_model.dart';
 import 'package:nightwatch/models/models.dart';
 
-class ReportsViewModel {
-  late ReportsRepository reportsRepository;
+class ReportsViewModel extends BaseViewModel {
+  late ReportsRepository _reportsService;
   late UserViewModel userViewModel;
-  late List<Report> userReports;
-  Report? tempReport;
+  late List<Report> _userReports;
+  Report? _newReport;
 
- ReportsViewModel() {
-   reportsRepository = ReportsService();
-   userViewModel = UserViewModel();
-   userReports = [];
- }
+  // Usage of this value is through a database
+  // event thus its null should not occur.
+  Report get newReport => _newReport!;
+  List<Report> get userReports => _userReports;
 
-  void postReport(Report report) {}
-  dynamic getReports() {}
-  dynamic getUserReports() {}
+  ReportsViewModel() {
+    _reportsService = serviceLocator<ReportsService>();
+    _reportsService.latestReport.listen(_latestUpdate);
+  }
+
+  void _latestUpdate(Report report) {
+    _newReport = report!; // Set the newReport for the UI
+    setState(ViewState.DataFetched); // Update the View
+  }
+
+  Future<void> getReports() async {
+    try {
+      setState(ViewState.Busy);
+      _userReports = await _reportsService.getReports();
+      setState(ViewState.DataFetched);
+    } on BackendlessException catch (_) {
+     // Error Dialog
+      setErrorDialog(error)
+    }
+  }
+
+  Future<void> postReport(Report report) async {
+    try {
+       await _reportsService.storeReport(report);
+       setState(ViewState.Success);
+    } catch (_) {
+
+    }
+  }
+  Future<void> getUserReports() async {
+    try {
+      setState(ViewState.Busy);
+      _userReports = await _reportsService.getUserReports();
+      setState(ViewState.Success);
+    } on BackendlessException catch(_) {
+
+    }
+  }
+
+  //TODO: Implement Notification View Model
   void alertUsers() {}
 }
