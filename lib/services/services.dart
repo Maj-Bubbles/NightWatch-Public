12879fd2f38e9f6e5f8d03413117f62bb25b5b9e
kamva9697 <kamva9697@gmail.com>
2022-11-19 13:30:21 +0200

    refactor: Add testable services

--- lib/services/services.dart
+++ lib/services/services.dart
@@ -1,3 +1,4 @@
+import 'package:backendless_sdk/backendless_sdk.dart';
 import 'package:get_it/get_it.dart';
 import 'package:nightwatch/repositories/apis/backendless_apis.dart';
 import 'package:nightwatch/services/reports_service.dart';
@@ -15,7 +16,7 @@ GetIt serviceLocator = GetIt.instance;
 
 void setUpServiceLocator() {
   // Register the Services to as a lazy singleton object.
-  serviceLocator.registerLazySingleton<ReportsService>(() => ReportsService(BackendlessDatabaseApi()));
+  serviceLocator.registerLazySingleton<ReportsService>(() => ReportsService(databaseApi: BackendlessDatabaseApi(), realTimeAPI: BackendlessRealTimeAPI(reportsHandler: Backendless.data.of("Reports").rt())));
   serviceLocator.registerLazySingleton<UserService>(() => UserService(BackendlessUserApi()));
   serviceLocator.registerLazySingleton<NavigationAndDialogService>(
       () => NavigationAndDialogService());
