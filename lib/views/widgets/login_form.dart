1b1eedc74e3675f566e7e3e318ac03d0d0378049
Emile <emilebotes01@icloud.com>
2022-11-26 02:50:23 +0200

    Feat: LoadingIndicatorV2 in login_form.dart - Stack with logo and blocks

--- lib/views/widgets/login_form.dart
+++ lib/views/widgets/login_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:loading_animations/loading_animations.dart';
 import 'package:nightwatch/miscellaneous/constants.dart';
 import 'package:nightwatch/miscellaneous/validators.dart';
 import 'package:nightwatch/routes/route_manager.dart';
@@ -205,6 +206,64 @@ class _LoginFormState extends State<LoginForm> {
             },
           ),
         ],
+      ),
+    );
+  }
+}
+
+class LoadingIndicatorV2 extends StatelessWidget {
+  const LoadingIndicatorV2({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        color: Colors.black.withOpacity(0.6),
+        child: Center(
+          child: SizedBox(
+            height: 175,
+            width: 300,
+            child: Card(
+              color: scaffoldBackgroundColor,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 10),
+                  const Text(
+                    'Logging you in',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  const SizedBox(height: 20),
+                  Stack(
+                    children: [
+                      const Image(
+                        image: AssetImage('media/Splash_GitHub_Page.png'),
+                        height: 75,
+                      ),
+                      LoadingBouncingGrid.square(
+                        backgroundColor: orangePeelForIconsAndButtons,
+                        size: 75,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Text(
+                    'Please wait',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  const SizedBox(height: 10),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
